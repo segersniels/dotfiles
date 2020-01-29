1d4a59de5e6026aa783f3dc64fb747f49793d036
segersniels <segers.n@hotmail.com>
2020-01-29 16:53:16 +0100

    :sparkles: Add hyper sync settings

--- finalize.sh
+++ finalize.sh
@@ -2,3 +2,4 @@
 cp .vscode-settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
 cp .zshrc ${HOME}/.zshrc
 cp .vimrc ${HOME}/.vimrc
+mkdir -p ${HOME}/.hyper_plugins && cp .hyper-sync-settings.json ${HOME}/.hyper_plugins/.hyper-sync-settings.json
