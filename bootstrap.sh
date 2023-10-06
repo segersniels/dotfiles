322f4d811916a4f920a398cf98e2842142bd1cc7
segersniels <segers.n@hotmail.com>
2023-10-06 16:29:51 +0200

    :sparkles: Install rust through rustup

--- bootstrap.sh
+++ bootstrap.sh
@@ -81,6 +81,7 @@ npm install --global --silent yarn
 npm install --global --silent pnpm
 
 # Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install propr
 cargo install supdock
 
